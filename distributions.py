e2850f63a0f2e355b60618769b2912cfe14d0c08
alinooralian <alinooralian.85@gmail.com>
2026-07-10 02:51:10 +0330

    Adding the Binomial distribution

--- distributions.py
+++ distributions.py
@@ -42,3 +42,35 @@ class Bernoulli(BaseDistribution):
 
     def theoretical_variance(self):
         return self.p * (1 - self.p)
+
+
+class Binomial(BaseDistribution):
+    def __init__(self, engine, n, p):
+        super().__init__(engine)
+
+        if n >= 0:
+            self.n = n
+        else:
+            raise ValueError("This number must be positive.")
+
+        if 0 <= p <= 1:
+            self.p = p
+        else:
+            raise ValueError("The probability must be between 0 and 1.")
+
+        self.bernoulli = Bernoulli(self.engine, self.p)
+
+    def generate_sample(self):
+        cnt = 0
+
+        for _ in range(self.n):
+            cnt += self.bernoulli.generate_sample()
+
+        return cnt
+
+    def theoretical_mean(self):
+        return self.n * self.p
+
+    def theoretical_variance(self):
+        return self.n * self.p * (1 - self.p)
+
