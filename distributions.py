87cbca9b6487c3d2725e98f373c7121d29e95e3c
alinooralian <alinooralian.85@gmail.com>
2026-07-10 02:50:41 +0330

    Adding the Bernoulli distribution

--- distributions.py
+++ distributions.py
@@ -19,3 +19,26 @@ class BaseDistribution(ABC):
     def theoretical_variance(self):
         pass
 
+
+class Bernoulli(BaseDistribution):
+    def __init__(self, engine: RandomNumberGenerator, p):
+        super().__init__(engine)
+
+        if 0 <= p <= 1:
+            self.p = p
+        else:
+            raise ValueError("The probability must be between 0 and 1.")
+
+    def generate_sample(self):
+        u = self.engine.create()
+
+        if u < self.p:
+            return 1
+
+        return 0
+
+    def theoretical_mean(self):
+        return self.p
+
+    def theoretical_variance(self):
+        return self.p * (1 - self.p)
