202ca196cb701d1aab6cc6ae381b8fb4d6c3952f
alinooralian <alinooralian.85@gmail.com>
2026-07-10 02:47:49 +0330

    Adding XorShift engine

--- random_number_generator.py
+++ random_number_generator.py
@@ -22,3 +22,21 @@ class LinearCongruentialGenerator(RandomNumberGenerator):
     def create(self):
         self.x = (self.coef * self.x) % self.mod
         return self.x / self.mod
+
+
+class XorShift(RandomNumberGenerator):
+    mask = 0xFFFFFFFF
+
+    def __init__(self):
+        super().__init__()
+        self.x = self.seed & self.mask
+
+        if self.x == 0:
+            self.x = 0x99999999
+
+    def create(self):
+        self.x ^= (self.x << 13)
+        self.x ^= (self.x >> 17)
+        self.x ^= (self.x << 5)
+
+        return (self.x & self.mask) / self.mask
