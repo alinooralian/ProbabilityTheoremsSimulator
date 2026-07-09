44047782486a7bfb0d5b482988d9d2fb4dd7249e
alinooralian <alinooralian.85@gmail.com>
2026-07-10 02:54:51 +0330

    Calculation of the empirical mean, variance, and percentage error

--- analysis.py
+++ analysis.py
@@ -8,3 +8,26 @@ def genrete_samples(distributions: BaseDistribution, M):
         samples.append(distributions.generate_sample())
 
     return samples
+
+
+def empirical_mean(samples):
+    ans = 0
+
+    for x in samples:
+        ans += x
+
+    return ans / len(samples)
+
+
+def empirical_variance(samples):
+    mean = empirical_mean(samples)
+    ans = 0
+
+    for x in samples:
+        ans += (x - mean) ** 2
+
+    return ans / (len(samples) - 1)
+
+
+def percent_error(theorical, empirical):
+    return abs((theorical - empirical) / theorical) * 100
